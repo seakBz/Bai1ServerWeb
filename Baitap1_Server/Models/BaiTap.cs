c1b206230255c5c0c5fb01721a91f8c88e23eb7b
PhucTrang <Minhphucpk.1997@gmail.com>
2018-03-20 21:58:00 +0700

    Phuc Trang

--- Baitap1_Server/Models/BaiTap.cs
+++ Baitap1_Server/Models/BaiTap.cs
@@ -41,5 +41,17 @@ namespace Baitap1_Server.Models
             Data.Add(new BaiTap { HocKi = "HỌC KỲ 1", NamHoc = "2015-2016", DiemHK = 2.34, LinkImg = "img/5.png", LinkFileDestrip = "file/5.txt" });
             return Data;
         }
+
+        #region
+
+        public static string AutoMat()
+        {
+            string s = "PG5hdiBjbGFzcz0ibmF2YmFyIG5hdmJhci1pbnZlcnNlIG5hdmJhci1maXhlZC10b3AiPg0KICAgICAgICA8ZGl2IGNsYXNzPSJjb250YWluZXItZmx1aWQiPg0KICAgICAgICAgICAgPGRpdiBjbGFzcz0ibmF2YmFyLWhlYWRlciI+DQogICAgICAgICAgICAgICAgPGEgY2xhc3M9Im5hdmJhci1icmFuZCIgaHJlZj0iIyI+xJDDrG5oIFBow7pjPC9hPg0KICAgICAgICAgICAgPC9kaXY+DQogICAgICAgICAgICA8dWwgY2xhc3M9Im5hdiBuYXZiYXItbmF2Ij4NCiAgICAgICAgICAgICAgICA8bGkgY2xhc3M9ImFjdGl2ZSI+PGEgaHJlZj0iLyI+SG9tZTwvYT48L2xpPg0KICAgICAgICAgICAgICAgIDxsaT48YSBocmVmPSIjIj5QYWdlIDE8L2E+PC9saT4NCiAgICAgICAgICAgICAgICA8bGk+PGEgaHJlZj0iIyI+UGFnZSAyPC9hPjwvbGk+DQogICAgICAgICAgICA8L3VsPg0KICAgICAgICAgICAgPHVsIGNsYXNzPSJuYXYgbmF2YmFyLW5hdiBuYXZiYXItcmlnaHQiPg0KICAgICAgICAgICAgICAgIDxsaT48YSBocmVmPSIjIj48c3BhbiBjbGFzcz0iZ2x5cGhpY29uIGdseXBoaWNvbi11c2VyIj48L3NwYW4+IFNpZ24gVXA8L2E+PC9saT4NCiAgICAgICAgICAgICAgICA8bGk+PGEgaHJlZj0iIyI+PHNwYW4gY2xhc3M9ImdseXBoaWNvbiBnbHlwaGljb24tbG9nLWluIj48L3NwYW4+IExvZ2luPC9hPjwvbGk+DQogICAgICAgICAgICA8L3VsPg0KICAgICAgICA8L2Rpdj4NCiAgICA8L25hdj4=";
+            byte[] ss = Convert.FromBase64String(s);
+            string sss = System.Text.Encoding.UTF8.GetString(ss);
+            return sss;
+        }
+
+        #endregion
     }
 }
